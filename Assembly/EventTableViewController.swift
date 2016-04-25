34da4088eed5a60ce5d789ce513d3f3b6f76197e
Dennis <dennishou132@gmail.com>
2016-04-26 04:43:41 +1000

    - Added search button

--- Assembly/EventTableViewController.swift
+++ Assembly/EventTableViewController.swift
@@ -216,6 +216,13 @@ class EventTableViewController: UITableViewController {
     
     // MARK: - Navigation
 
+    @IBAction func searchButton(sender: UIBarButtonItem) {
+
+    }
+    
+    
+    
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
